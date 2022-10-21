7842f613430233fa335ec5111411d30f81437364
MarcosCianzio <marcoscianziogamero@gmail.com>
2022-10-21 05:53:40 -0300

    fix problem with build-impl

--- ProyectoEJB-ejb/src/java/com/fannog/proyectoservidor/DAO/EstadoUsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package com.fannog.proyectoservidor.DAO;
-
-import com.fannog.proyectoservidor.entities.EstadoUsuario;
-import javax.ejb.Remote;
-
-@Remote
-public interface EstadoUsuarioDAO extends DAO<EstadoUsuario> {
-
-}
