bee2a17347433bfc8e22ac1ba7751886ed971a6c
MarcosCianzio <marcoscianziogamero@gmail.com>
2022-10-21 05:54:31 -0300

    fix problem cannot link

---
+++ ProyectoEJB-ejb/src/java/com/fannog/proyectoservidor/DAO/impl/EstadoUsuarioDAOImpl.java
@@ -0,0 +1,76 @@
+package com.fannog.proyectoservidor.DAO.impl;
+
+import com.fannog.proyectoservidor.DAO.EstadoUsuarioDAO;
+import com.fannog.proyectoservidor.entities.EstadoUsuario;
+import com.fannog.proyectoservidor.exceptions.ServicioException;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+@Stateless
+public class EstadoUsuarioDAOImpl implements EstadoUsuarioDAO {
+
+    @PersistenceContext(unitName = "ProyectoEJB-ejbPU")
+    private EntityManager em;
+
+    @Override
+    public EstadoUsuario create(EstadoUsuario estadoUsuario) throws ServicioException {
+        try {
+            EstadoUsuario yaExiste = (EstadoUsuario) em.createNamedQuery("EstadoUsuario.findByNombre").setParameter("nombre", estadoUsuario.getNombre()).getSingleResult();
+
+            if (yaExiste != null) {
+                throw new ServicioException("Ya existe un estado de usuario con este nombre");
+            }
+        } catch (NoResultException e) {
+        }
+
+        try {
+            em.persist(estadoUsuario);
+            em.flush();
+        } catch (PersistenceException e) {
+            throw new ServicioException("Ha ocurrido un error al intentar crear el estado de usuario");
+        }
+
+        return estadoUsuario;
+    }
+
+    @Override
+    public EstadoUsuario edit(EstadoUsuario estadoUsuario) throws ServicioException {
+        try {
+            em.merge(estadoUsuario);
+            em.flush();
+        } catch (PersistenceException e) {
+            throw new ServicioException("Ha ocurrido un error al intentar actualizar el estado de usuario");
+        }
+
+        return estadoUsuario;
+    }
+
+    @Override
+    public void remove(EstadoUsuario estadoUsuario) throws ServicioException {
+        try {
+            em.remove(estadoUsuario);
+            em.flush();
+        } catch (PersistenceException e) {
+            throw new ServicioException("Ha ocurrido un error al intentar dar de baja el estado de usuario");
+        }
+    }
+
+    @Override
+    public EstadoUsuario findById(Long id) {
+        EstadoUsuario estadoUsuario = em.find(EstadoUsuario.class, id);
+
+        return estadoUsuario;
+    }
+
+    @Override
+    public List<EstadoUsuario> findAll() {
+        List<EstadoUsuario> estadosUsuario = em.createNamedQuery("EstadoUsuario.findAll").getResultList();
+
+        return estadosUsuario;
+    }
+
+}
